2efaa4093c5f97b985ad678146eccfc01c99b85c
Lameck Kipruto <lameckkipruto1@.com>
2023-06-15 09:29:23 -0700

    task 5 printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * main is the start point
+ * main - is the start point
  *
- * return value is 0
+ * Return -  value is 0
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
