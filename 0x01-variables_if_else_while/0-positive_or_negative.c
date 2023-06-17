f09a306137c46bbd9d07a64b38f0555ba39c1292
Lameck Kipruto <lameckkipruto1@.com>
2023-06-16 17:52:30 -0700

    task 0 positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,10 @@
 #include<stdlib.h>
 /**
 * main - Prints if number is positive, zero or negative
-* Return: Always (Success)
 *
+* Return: Always (Success)
 * /
-* int main(void)
+int main(void)
 {
 	int n;
 
