a108c95208cc27a52a48db62d46793c3de1cfea4
Lameck Kipruto <lameckkipruto1@.com>
2023-06-20 11:11:32 -0700

    task 1 alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /*
  * main  -  entry point
+ * print_alphabet() - function using a for loop to prints letters of alphabet
  * return:usually 0
  */
 void print_alphabet(void)
---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int a = 8;
+int b = 5;
+int result;
+int add(int a, int b)
+{
+	result = a+b;i
+	return (result);
+}
+int main(void)
+{
+	printf("%d", &result);
+	return (0);
+}
