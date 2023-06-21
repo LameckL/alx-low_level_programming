3747e28775f1e248ac0682ccc62f5c5bf452fb5c
Lameck Kipruto <lameckkipruto1@.com>
2023-06-21 08:29:12 -0700

    task 0 putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - entry point
  * return: always 0
  */
 int main(void)
 {
-	printf("\_putchar");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
