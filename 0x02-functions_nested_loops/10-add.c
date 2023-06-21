020b91674e862706a8959a9c0047c65b5d17ec13
Lameck Kipruto <lameckkipruto1@.com>
2023-06-21 08:23:50 -0700

    task 0 putchar

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,16 +4,7 @@
  *
  * Return: Always 0.
  */
-int a = 8;
-int b = 5;
-int result;
 int add(int a, int b)
 {
-	result = a+b;i
-	return (result);
-}
-int main(void)
-{
-	printf("%d", &result);
-	return (0);
+	return (a + b);
 }
