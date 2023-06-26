6ba2e16354bf979bdd938a4759fddcbfe2fc8df2
Lameck Kipruto <lameckkipruto1@.com>
2023-06-26 06:17:28 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - this function takes a pointer to an int as a parameter
+ * it then updates the value it points to to 98
+ * @n: this is an input
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
