72674d0e612fb37687412dcb4eefc3ed68f034c2
Lameck Kipruto <lameckkipruto1@.com>
2023-07-05 09:37:36 -0700

    task 4 raised to power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
- * _pow_recursion - functio that returns the value of x raised to the power of y
+ * _pow_recursion - function returning value of x raised to power of y
  * @x: value to raise
  * @y: power
- *
  * Return: result of the power
  */
 int _pow_recursion(int x, int y)
