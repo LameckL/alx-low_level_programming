2544e06f306a8c29a59f9d0a2d91b3376eb8666e
Lameck Kipruto <lameckkipruto1@.com>
2023-07-19 05:39:23 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,6 +4,7 @@
  * print_name - function to print name using pointer to function
  * @name: a string to add
  * @f: pointer to function
+ * Return: none
  **/
 void print_name(char *name, void (*f)(char *))
 {
