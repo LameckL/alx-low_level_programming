28141d3ce8c01a96ac6f2e1f54ed8740e894ecc3
Lameck Kipruto <lameckkipruto1@.com>
2023-08-01 10:06:14 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,33 +11,33 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *new;
-	listint_t *temp = *head;
+	listint_t *newn;
+	listint_t *tmp = *head;
 
 	new = malloc(sizeof(listint_t));
-	if (!new || !head)
+	if (!newn || !head)
 		return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+	newn->n = n;
+	newn->next = NULL;
 
 	if (idx == 0)
 	{
 		new->next = *head;
-		*head = new;
-		return (new);
+		*head = newn;
+		return (newn);
 	}
 
-	for (i = 0; temp && i < idx; i++)
+	for (i = 0; tmp && i < idx; i++)
 	{
 		if (i == idx - 1)
 		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
+			new->next = tmp->next;
+			tmp->next = newn;
+			return (newn);
 		}
 		else
-			temp = temp->next;
+			tmp = tmp->next;
 	}
 
 	return (NULL);
