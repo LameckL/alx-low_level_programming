d579dda811eac6397f0602aa1b93374ffe6e028b
Lameck Kipruto <lameckkipruto1@.com>
2023-08-01 10:38:54 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,8 +9,8 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp = *head;
-	listint_t *current = NULL;
+	listint_t *tmp = *head;
+	listint_t *present = NULL;
 	unsigned int i = 0;
 
 	if (*head == NULL)
@@ -19,22 +19,22 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		free(temp);
+		free(tmp);
 		return (1);
 	}
 
 	while (i < index - 1)
 	{
-		if (!temp || !(temp->next))
+		if (!tmp || !(tmp->next))
 			return (-1);
-		temp = temp->next;
+		tmp = tmp->next;
 		i++;
 	}
 
 
-	current = temp->next;
-	temp->next = current->next;
-	free(current);
+	present = tmp->next;
+	tmp->next = present->next;
+	free(present);
 
 	return (1);
 }
