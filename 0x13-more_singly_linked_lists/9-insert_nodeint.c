03792d06f5e57ae21a83def22e725e15ab2d83e3
Lameck Kipruto <lameckkipruto1@.com>
2023-08-01 10:24:39 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new->next = *head;
+		newn->next = *head;
 		*head = newn;
 		return (newn);
 	}
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (i == idx - 1)
 		{
-			new->next = tmp->next;
+			newn->next = tmp->next;
 			tmp->next = newn;
 			return (newn);
 		}
