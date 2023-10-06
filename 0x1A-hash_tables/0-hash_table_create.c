97a4810896ed6a7868b6a01ae49e4ccf940a3239
LameckL <88710250+LameckL@users.noreply.github.com>
2023-10-06 11:28:19 +0100

    creating hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - func creating a hash table
+ * @size: array size
+ *
+ * Return: the pointer to the new has table, else
+ *         NULL - error
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hasht;
+	unsigned long int i;
+
+	hasht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	hasht->size = size;
+	hasht->array = malloc(sizeof(hash_node_t *) * size);
+	if (hashtt->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		hasht->array[i] = NULL;
+
+	return (hasht);
+}
