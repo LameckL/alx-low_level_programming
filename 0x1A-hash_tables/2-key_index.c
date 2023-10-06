350c7c41b35ad5f0fbea6ddc89e48283de9934a1
LameckL <88710250+LameckL@users.noreply.github.com>
2023-10-06 12:20:59 +0100

    task 2,3

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - func ging index of a key
+ * @key: te key to give index
+ * @size: array size
+ * Return: key index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
