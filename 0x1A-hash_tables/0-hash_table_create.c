aaf75955d87188089c6e90691ee8112b71c50061
LameckL <88710250+LameckL@users.noreply.github.com>
2023-10-06 11:45:03 +0100

    fixing 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hasht->size = size;
 	hasht->array = malloc(sizeof(hash_node_t *) * size);
-	if (hashtt->array == NULL)
+	if (hasht->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		hasht->array[i] = NULL;
